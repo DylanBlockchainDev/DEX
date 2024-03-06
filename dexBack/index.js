93489d86ce586fc093320cd6928a7582107f0c89
Dylan <dpkatsch@gmail.com>
2024-03-06 10:02:03 +0200

    discard

--- dexBack/index.js
+++ dexBack/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-require("dotenv").config();
+// require("dotenv").config();
 // const port = 3001;
 
 app.use(express.json());
